39df625ecd4ae450313ee2872110a97836200ef2
Nathan <nathanmerry9713@gmail.com>
2020-11-06 22:45:36 +0000

    update pages migration

--- database/migrations/2020_10_19_162603_create_pages_table.php
+++ database/migrations/2020_10_19_162603_create_pages_table.php
@@ -14,11 +14,10 @@ class CreatePagesTable extends Migration
     public function up()
     {
         Schema::create('pages', function (Blueprint $table) {
-            $table->string('how_it_works')->nullable();
-            $table->string('faq')->nullable();
-            $table->string('scam_watch')->nullable();
-            $table->string('opt_out')->nullable();
-            $table->string('apply')->nullable();
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('slug')->nullable();
+            $table->string('content')->nullable();
         });
     }
 
