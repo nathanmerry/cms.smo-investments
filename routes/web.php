52106824db49b56894ef42f2f66e550904188fa1
Nathan <nathanmerry9713@gmail.com>
2020-11-07 09:33:48 +0000

    convert routes to group middleware

--- routes/web.php
+++ routes/web.php
@@ -5,48 +5,25 @@ use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-// Websites
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    // Websites
+    Route::get('/websites', [WebsiteController::class, 'index'])->name('websites');
+    Route::get('/websites/{website}/edit', [WebsiteController::class, 'show'])->name('website');;
+    Route::post('/websites/save', [WebsiteController::class, 'update']);
+    Route::post('/websites/create', [WebsiteController::class, 'create'])->name('create');
+    Route::get('/websites/store', [WebsiteController::class, 'store']);
 
-// Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-//     Route::get('/websites', [WebsiteController::class, 'index']);
-// });
+    // companies
+    Route::get('/companies', [CompanyController::class, 'index'])->name('companies');
+    Route::get('/companies/{company}', [CompanyController::class, 'show'])->name('company');
+    Route::post('/companies/save', [CompanyController::class, 'update']);
 
-Route::middleware(['auth:sanctum', 'verified'])
-    ->get('/websites', [WebsiteController::class, 'index'])
-    ->name('websites');
+    // Pages
+    Route::get('/pages', [PageController::class, 'index'])->name('pages');
+    Route::get('/pages/{page}', [PageController::class, 'show'])->name('page');
+    Route::post('/pages/save', [PageController::class, 'update']);
+});
 
-Route::middleware(['auth:sanctum', 'verified'])
-    ->get('/websites/{website}/edit', [WebsiteController::class, 'show'])
-    ->name('website');
-
-Route::middleware(['auth:sanctum', 'verified'])
-    ->post('/websites/save', [WebsiteController::class, 'update']);
-
-// Route::get('/websites/{website}/edit', [WebsiteController::class, 'show'])->name('website');;
-// Route::post('/websites/save', [WebsiteController::class, 'update']);
-
-Route::middleware(['auth:sanctum', 'verified'])
-    ->get('/websites/create', [WebsiteController::class, 'create'])
-    ->name('create');
-
-Route::middleware(['auth:sanctum', 'verified'])
-    ->post('/websites/store', [WebsiteController::class, 'store']);
-
-// Route::get('/websites/create', [WebsiteController::class, 'create'])->name('create');
-Route::post('/websites/store', [WebsiteController::class, 'store']);
-
-// Route::post('/websites/slug', [WebsiteController::class, 'slug']);
-
-
-// Companies
-Route::get('/companies', [CompanyController::class, 'index'])->name('companies');
-Route::get('/companies/{company}', [CompanyController::class, 'show'])->name('company');
-Route::post('/companies/save', [CompanyController::class, 'update']);
-
-// Pages
-Route::get('/pages', [PageController::class, 'index'])->name('pages');
-Route::get('/pages/{page}', [PageController::class, 'show'])->name('page');
-Route::post('/pages/save', [PageController::class, 'update']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
     return view('dashboard');
