a0b9568eaa02b6a3928869992c31379084106d3f
Nathan <nathanmerry9713@gmail.com>
2020-11-07 22:11:03 +0000

    add page seeder

---
+++ database/seeders/PageSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Page;
+
+class PageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Page::updateOrCreate([
+            'name' => "Scams",
+            'slug' => 'scams',
+            'content' => 'Scams content',
+        ]);
+
+        Page::updateOrCreate([
+            'name' => 'How It Works',
+            'slug' => 'how-it-works',
+            'content' => 'How it works content',
+        ]);
+        Page::updateOrCreate([
+            'name' => "FAQ's",
+            'slug' => 'faqs',
+            'content' => 'FAQs content',
+        ]);
+    }
+}
