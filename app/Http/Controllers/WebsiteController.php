5e4600575f23af01a029140701a1761f77dedd18
Nathan <nathanmerry9713@gmail.com>
2020-11-07 18:16:09 +0000

    add logo upload

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -7,56 +7,60 @@ use App\Models\Website;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use App\Models\Company;
 
-class WebsiteController extends Controller
+
+class Websitecontroller extends Controller
 {
-    protected $ipAddress = "167.99.194.56";
+    protected $ipaddress = "167.99.194.56";
 
     /**
-     * Display a listing of the resource.
+     * display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \illuminate\http\response
      */
     public function index()
     {
-        $query = Website::all();
+        $query = website::all();
 
         $data = $query->map(function ($item) {
             return [
-                'name' => $item->{'Website_Name'},
-                'slug' => str_replace(' ', '-', strtolower($item->{'Website_Name'}))
+                'name' => $item->website_name,
+                'slug' => $item->website_slug
             ];
         });
 
         return view('websites.index', ['data' => $data]);
     }
 
-    public function checkFtpLogin(Request $request)
+    public function checkftplogin(request $request)
     {
-        $conn_id = ftp_connect($this->ipAddress);
+        $conn_id = ftp_connect($this->ipaddress);
         if (@ftp_login($conn_id, $request->ftpUser, $request->ftpPassword)) {
             return [
                 'success' => true,
-                'header' => 'FTP connection secured',
+                'header' => 'ftP connection secured',
                 'message' => 'Please press continue if you wish to upload the files to <span class="font-bold">' . $request->ftpUser . '</span>',
             ];
         } else {
             return [
                 'success' => false,
-                'header' => 'FTP connection not secured',
-                'message' => 'Please try again with different login details, or check your FTP account.',
+                'header' => 'ftP connection not secured',
+                'message' => 'Please try again with different login details, or check your ftP account.',
             ];
         }
         ftp_close($conn_id);
     }
 
-    public function storeNewSiteFiles(Request $request)
+    public function storenewsitefiles(request $request)
     {
-        $ftp = new \FtpClient\FtpClient();
-        $ftp->connect($this->ipAddress);
+        $ftp = new \ftpclient\ftpclient();
+        $ftp->connect($this->ipaddress);
         $ftp->login($request->ftpUser, $request->ftpPassword);
 
-        $sourceFile = __DIR__ . '/../../../newSiteFiles/' . $request->file;
+        $sourcefile = __DIR__ . '/../../../newsitefiles/' . $request->file;
 
         if ($request->file === '.' || $request->file === '..') {
             return [
@@ -64,145 +68,154 @@ class WebsiteController extends Controller
             ];
         }
 
-        if (is_file($sourceFile)) {
+        if (is_file($sourcefile)) {
             $ftp->chdir('/public_html');
-            $ftp->put($request->file, $sourceFile);
+            $ftp->put($request->file, $sourcefile);
         } else {
             $ftp->mkdir('/public_html' . '/' . $request->file);
-            $ftp->putAll($sourceFile, '/public_html/' . $request->file);
+            $ftp->putall($sourcefile, '/public_html/' . $request->file);
         }
 
-        $uploaded = array_column($ftp->scanDir('/public_html'), 'name');
+        $uploaded = array_column($ftp->scandir('/public_html'), 'name');
 
         return ['files' => $uploaded];
     }
 
     /**
-     * Store a newly created resource in storage.
+     * store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \illuminate\http\request  $request
+     * @return \illuminate\http\response
      */
-    public function store(Request $request)
+    public function store(request $request)
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'slug' => 'required|unique:websites,Website_Slug',
+            'slug' => 'required|unique:websites,website_slug',
         ]);
 
         if ($validator->fails()) {
             return [
                 'success' => false,
-                'header' => 'Upload Failed',
+                'header' => 'Upload failed',
                 'message' => 'This website already exists! Please choose another one and ensure the slug is unique.',
             ];
         }
 
-        $saved = Website::create([
-            'Website_Name' => $request->name,
-            'Website_Slug' => $request->slug
+        $saved = website::create([
+            'website_name' => $request->name,
+            'website_slug' => $request->slug
         ]);
 
         if ($saved) {
             return [
                 'success' => true,
-                'message' => 'Website Saved to CMS'
+                'message' => 'website saved to cMs'
             ];
         } else {
             return [
                 'success' => false,
-                'message' => 'Could not save website to CMS!',
+                'message' => 'could not save website to cMs!',
             ];
         }
     }
 
-    public function create(Request $request)
+    public function create(request $request)
     {
-        $query = Website::all();
+        $query = website::all();
 
         $websites = $query->map(function ($item) {
             return [
-                'name' => $item->{'Website_Name'},
-                'slug' => $item->{'Website_Slug'}
+                'name' => $item->{'website_name'},
+                'slug' => $item->{'website_slug'}
             ];
         });
 
-        $ftpFiles = scandir(__DIR__ . '/../../../newSiteFiles');
-
+        
+        $ftpFiles = scandir(__DIR__ . '/../../../newsitefiles');
+        
         return view('websites.create', ['websites' => $websites, 'ftpFiles' => $ftpFiles]);
     }
 
     /**
-     * Display the specified resource.
+     * display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \illuminate\http\response
      */
     public function show($slug)
     {
-        $query = DB::table('Websites');
+        $query = DB::table('website');
 
         $websites = $query->get()->map(function ($item) {
             return [
-                'name' => $item->{'Website_Name'},
-                'slug' => $item->Website_Slug
+                'name' => $item->{'website_name'},
+                'slug' => $item->website_slug
             ];
         });
 
-        $website = $query->where('Website_Slug', '=', $slug)->get()->first();
-
+        $company = Company::all();
+        
+        $website = $query->where('website_slug', '=', $slug)->get()->first();
+        
         if (!$website) {
             abort(404);
         }
 
         $form = [
             'website' => [
-                'Website_Name' => $website->{'Website_Name'},
-                'Website_Address' => $website->{'Website_Address'},
-                'Website_Short_Address' => $website->{'Website_Short_Address'},
+                'website_name' => $website->{'website_name'},
+                'website_address' => $website->{'website_address'},
+                'website_short_address' => $website->{'website_short_address'},
             ],
             'color' => [
-                "Brand_Colour" => $website->{'Brand_Colour'},
-                "Header_Colour" => $website->{'Header_Colour'},
-                "Header_Font_Colour" => $website->{'Header_Font_Colour'},
-                "Header_Font_Colour_Hover" => $website->{'Header_Font_Colour_Hover'},
-                "Footer_Background_Colour" => $website->{'Footer_Background_Colour'},
-                "Footer_Font_Colour" => $website->{'Footer_Font_Colour'},
-                "Second_Colour" => $website->{'Second_Colour'},
-                "Homepage_Block_Colour" => $website->{'Homepage_Block_Colour'},
-                "Warning_Block" => $website->{'Warning_Block'},
-                "Homepage_Heading_Colour" => $website->{'Homepage_Heading_Colour'},
-                "Homepage_Span_Colour" => $website->{'Homepage_Span_Colour'},
-                "Homepage_Reasons_Colour" => $website->{'Homepage_Reasons_Colour'},
-                "Homepage_Block_Border" => $website->{'Homepage_Block_Border'},
-                "Button_Colour" => $website->{'Button_Colour'},
-                "Button_Colour_Border" => $website->{'Button_Colour_Border'},
-                "Button_Colour_Hover" => $website->{'Button_Colour_Hover'},
-                "Slider_Colour" => $website->{'Slider_Colour'},
-                "Loan_Information_Colour" => $website->{'Loan_Information_Colour'}
+                "brand_colour" => $website->{'brand_colour'},
+                "header_colour" => $website->{'header_colour'},
+                "header_font_colour" => $website->{'header_font_colour'},
+                "header_font_colour_hover" => $website->{'header_font_colour_hover'},
+                "footer_background_colour" => $website->{'footer_background_colour'},
+                "footer_font_colour" => $website->{'footer_font_colour'},
+                "second_colour" => $website->{'second_colour'},
+                "homepage_block_colour" => $website->{'homepage_block_colour'},
+                "warning_block" => $website->{'warning_block'},
+                "homepage_heading_colour" => $website->{'homepage_heading_colour'},
+                "homepage_span_colour" => $website->{'homepage_span_colour'},
+                "homepage_reasons_colour" => $website->{'homepage_reasons_colour'},
+                "homepage_block_border" => $website->{'homepage_block_border'},
+                "button_colour" => $website->{'button_colour'},
+                "button_colour_border" => $website->{'button_colour_border'},
+                "button_colour_hover" => $website->{'button_colour_hover'},
+                "slider_colour" => $website->{'slider_colour'},
+                "loan_information_colour" => $website->{'loan_information_colour'}
             ],
             'ctas' => [
-                "Home_CTA_One" => $website->{'Home_CTA_One'},
-                "Home_CTA_Two" => $website->{'Home_CTA_Two'},
-                "Home_CTA_Three" => $website->{'Home_CTA_Three'}
+                "home_cta_one" => $website->{'home_cta_one'},
+                "home_cta_two" => $website->{'home_cta_two'},
+                "home_cta_three" => $website->{'home_cta_three'}
             ],
             'images' => [
-                "Home_Image_One" => $website->{'Home_Image_One'},
-                "Home_Image_Two" => $website->{'Home_Image_One'},
-                "Home_Image_Three" => $website->{'Home_Image_Three'},
+                "home_image_one" => $website->{'home_image_one'},
+                "home_image_two" => $website->{'home_image_one'},
+                "home_image_three" => $website->{'home_image_three'},
             ],
             'text' => [
-                "H1" => $website->{'H1'},
+                "h1" => $website->{'h1'},
                 "h2c" => $website->{'h2c'},
                 "h3c" => $website->{'h3c'},
-                "P" => $website->{'P'}
+                "P" => $website->{'p'}
             ],
-            'ID' => $website->{'ID'}
+            'ID' => $website->{'id'},
+            'logo_filename' => $website->logo_filename,
+            'logo_url' => $website->logo_url,
+            'company' => [
+                'all' => $company,
+                'selected' => $website->company,
+            ],
         ];
 
         return view('websites.show', ['data' => [
             'websites' => $websites,
-            'website' => $website->Website_Name,
+            'website' => $website->website_name,
             'form' => $form,
         ]]);
     }
@@ -210,47 +223,51 @@ class WebsiteController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \illuminate\http\request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \illuminate\http\response
      */
-    public function update(Request $request)
+    public function update(request $request)
     {
-        $query = DB::table('Websites')->where('ID', $request->get('ID'));
-
+        $query = Website::where('id', $request->get('ID'));
         $data = request()->except('_token');
 
-        foreach ($data as $key => $item) {
-            if (is_null($item)) {
-                $data[$key] = '&nbsp';
-            }
+        $logo = $request->file('logo_filename');
+
+        if ($logo) {
+            Storage::disk('public')->put(
+                $logo->getFilename() . '.' . $logo->getClientOriginalExtension(),
+                File::get($logo)
+            );
+
+            $data['logo_filename'] = $logo->getClientOriginalName();
+            $data['logo_url'] = url('uploads/' . $logo->getFilename() . '.' . $logo->getClientOriginalExtension());
         }
 
         $query->update($data);
 
-        Session::flash('message', $data['Website_Name'] . ' Updated Successfully !');
-
-        return redirect('websites/' . $query->get()->first()->Website_Slug . '/edit');
+        Session::flash('message', $data['website_name'] . ' Updated successfully !');
+        return redirect('websites/' . $query->get()->first()->website_slug . '/edit');
     }
 
-    public function slug(Request $request)
+    public function slug(request $request)
     {
-        $getQuery = DB::table('Websites')->get();
+        $getQuery = DB::table('website')->get();
 
         foreach ($getQuery as $key => $item) {
-            $slug = str_replace(' ', '-', strtolower($item->{'Website_Name'}));
+            $slug = str_replace(' ', '-', strtolower($item->{'website_name'}));
 
-            $query = DB::table('Websites')->where('Website_Name', $item->{'Website_Name'});
+            $query = DB::table('website')->where('website_name', $item->{'website_name'});
 
-            $query->update(['Website_Slug' => $slug]);
+            $query->update(['website_slug' => $slug]);
         }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \illuminate\http\response
      */
     public function destroy($id)
     {
