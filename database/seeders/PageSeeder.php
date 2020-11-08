e90bf18c1b7b8bd2169579b46c351d9736ef8b6f
Nathan <nathanmerry9713@gmail.com>
2020-11-08 10:57:37 +0000

    update page seeder

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Page;
+use Illuminate\Support\Facades\DB;
 
 class PageSeeder extends Seeder
 {
@@ -14,24 +15,28 @@ class PageSeeder extends Seeder
      */
     public function run()
     {
-        Page::updateOrCreate([
-            'id' => 0,
-            'name' => "Scams",
-            'slug' => 'scams',
-            'content' => 'Scams content',
-        ]);
+        DB::table('pages')->insert(
+            [
+                'name' => "Scams",
+                'slug' => 'scams',
+                'content' => 'Scams content',
+            ]
+        );
 
-        Page::updateOrCreate([
-            'id' => 1,
-            'name' => 'How It Works',
-            'slug' => 'how-it-works',
-            'content' => 'How it works content',
-        ]);
-        Page::updateOrCreate([
-            'id' => 2,
-            'name' => "FAQ's",
-            'slug' => 'faq',
-            'content' => 'FAQs content',
-        ]);
+        DB::table('pages')->insert(
+            [
+                'name' => 'How It Works',
+                'slug' => 'how-it-works',
+                'content' => 'How it works content',
+            ]
+        );
+
+        DB::table('pages')->insert(
+            [
+                'name' => "FAQ's",
+                'slug' => 'faq',
+                'content' => 'FAQs content',
+            ]
+        );
     }
 }
